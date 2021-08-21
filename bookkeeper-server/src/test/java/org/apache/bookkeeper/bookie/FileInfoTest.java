58c3fc338732505fca31c26062046d3de6abb659
Giacomo Lorenzo Rossi <giacomo.redjack@gmail.com>
2021-08-21 23:04:23 +0200

    Corretta versione jacoco

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
@@ -0,0 +1,100 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileInfoTest {
+
+    @Test
+    public void getLastAddConfirmed() {
+    }
+
+    @Test
+    public void setLastAddConfirmed() {
+    }
+
+    @Test
+    public void waitForLastAddConfirmedUpdate() {
+    }
+
+    @Test
+    public void cancelWaitForLastAddConfirmedUpdate() {
+    }
+
+    @Test
+    public void isClosed() {
+    }
+
+    @Test
+    public void getLf() {
+    }
+
+    @Test
+    public void getSizeSinceLastWrite() {
+    }
+
+    @Test
+    public void getExplicitLac() {
+    }
+
+    @Test
+    public void setExplicitLac() {
+    }
+
+    @Test
+    public void readHeader() {
+    }
+
+    @Test
+    public void isDeleted() {
+    }
+
+    @Test
+    public void checkOpen() {
+    }
+
+    @Test
+    public void isFenced() {
+    }
+
+    @Test
+    public void setFenced() {
+    }
+
+    @Test
+    public void flushHeader() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void read() {
+    }
+
+    @Test
+    public void close() {
+    }
+
+    @Test
+    public void write() {
+    }
+
+    @Test
+    public void moveToNewLocation() {
+    }
+
+    @Test
+    public void getMasterKey() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void isSameFile() {
+    }
+}
